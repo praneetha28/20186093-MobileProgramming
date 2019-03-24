5901dc58682380bf75e17465a31be596aaf76e95
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2019-03-24 12:43:36 +0530

    module6

---
+++ Module-6/Module-4/Activity-54/Exercise-AddTheViewModel/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
@@ -0,0 +1,31 @@
+package com.example.android.todolist;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskEntry;
+
+import java.util.List;
+
+// TODO (1) make this class extend AndroidViewModel and implement its default constructor
+public class MainViewModel extends AndroidViewModel{
+
+    private LiveData<List<TaskEntry>> tasks;
+    public MainViewModel(Application application) {
+        super(application);
+        AppDatabase database = AppDatabase.getInstance(this.getApplication());
+        tasks = database.taskDao().loadAllTasks();
+    }
+    public  LiveData<List<TaskEntry>> getTasks() {
+        return tasks;
+    }
+
+    // TODO (2) Add a tasks member variable for a list of TaskEntry objects wrapped in a LiveData
+
+    // TODO (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable
+
+    // TODO (3) Create a getter for the tasks variable
+
+}
--- Module-6/app/src/main/java/com/example/shoppingcart/Interface/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.shoppingcart.Interface;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void OnClick(View view, int position, boolean isLongClick);
-}
